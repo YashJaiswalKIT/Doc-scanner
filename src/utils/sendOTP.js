cf39cbb69d98dfd7b3e83a1cd2ea3bb07affc967
Yash Jaiswal <yash2304vns@gmail.com>
2025-09-12 18:55:59 +0530

    Update sendOTP.js

--- src/utils/sendOTP.js
+++ src/utils/sendOTP.js
@@ -6,7 +6,7 @@ export const sendOTP = async (email) => {
 
   try {
     
-    const response = await fetch("https://doc-scanner-backend.onrender.com/api/send-otp", {
+    const response = await fetch("https://doc-scanner-backend.onrender.com/send-otp", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email }),
