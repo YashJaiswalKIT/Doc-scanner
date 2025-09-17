cc59efd00509983c192f3a51735641c497e65ce2
Yash Jaiswal <yash2304vns@gmail.com>
2025-09-17 20:20:09 +0530

    Update ScanUserQR.jsx

--- src/Pages/ScanUserQR.jsx
+++ src/Pages/ScanUserQR.jsx
@@ -90,7 +90,7 @@ const ScanUserQR = () => {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email: ownerEmail, otp }),
+          body: JSON.stringify({ email: ownerEmail, otp :String(otp)}),
         }
       );
 
